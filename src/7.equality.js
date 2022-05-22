2c3c1c12b5f5202d17fe82fbac5ac6320e417129
belmonjd1 <belmonjd@mail.uc.edu>
2022-05-22 19:54:27 -0400

    Updated 7.equality.js
    I updated the 7.equality.js file and ensured that it passed all tests ran by the npm test command.

--- src/7.equality.js
+++ src/7.equality.js
@@ -2,14 +2,26 @@
 // Example: strictEquality(1, true) => false
 // Example: strictEquality(1, 1) => true
 function strictEquality(val1, val2) {
-  return ;
+  res = false;
+  
+  if (val1 === val2) {
+    res = true;
+  }
+
+  return res;
 }
 //equality: compare value but not necessarily the type
 // Example: equality(1, true) => true
 // Example: equality(1, 1) => true
 // Example: equality(1, false) => false
 function equality(val1, val2) {
-  return ;
+  res = false;
+  
+  if (val1 == val2) {
+    res = true;
+  }
+
+  return res;
 }
 
 module.exports = {
